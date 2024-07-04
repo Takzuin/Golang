dead26d46ee85e141e9b435c71535e50ba088072
TAKZUIN <Takzuin@gmail.com>
2024-07-04 15:30:55 -0500

    Update dataTypes.go

--- HelloGoBasics/dataTypes.go
+++ HelloGoBasics/dataTypes.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	fmt.Println("Text" + "More Text")
 	fmt.Println(2 + 8)
-	fmt.Println(2 / 8)
+	fmt.Println(8 / 4)
 	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 }
